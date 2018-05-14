d9ae135a372bec9921981dec85606744d827993f
TartaricAcid <baka943@qq.com>
2018-05-14 14:30:04 +0800

    Texture Push

---
+++ src/main/java/github/baka943/simplewine/block/CropsSoybean.java
@@ -0,0 +1,33 @@
+package github.baka943.simplewine.block;
+
+import github.baka943.simplewine.item.ItemLoader;
+import net.minecraft.block.*;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class CropsSoybean extends BlockCrops {
+    public CropsSoybean() {
+        this.setRegistryName("crops_soybean");
+        this.setDefaultState(this.blockState.getBaseState().withProperty(this.getAgeProperty(), Integer.valueOf(0)));
+        this.setTickRandomly(true);
+        this.setHardness(0F);
+        this.setSoundType(SoundType.PLANT);
+        this.disableStats();
+    }
+
+    protected Item getSeed() {
+        return ItemLoader.seedSoybean;
+    }
+
+    protected Item getCrop() {
+        return ItemLoader.seedSoybean;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(this.getRegistryName(), "inventory"));
+    }
+}
---
+++ src/main/java/github/baka943/simplewine/item/SeedSoybean.java
@@ -0,0 +1,55 @@
+package github.baka943.simplewine.item;
+
+import github.baka943.simplewine.SimpleWine;
+import github.baka943.simplewine.block.BlockLoader;
+import github.baka943.simplewine.proxy.CommonProxy;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class SeedSoybean extends Item implements net.minecraftforge.common.IPlantable {
+    public SeedSoybean() {
+        this.setRegistryName("seed_soybean");
+        this.setUnlocalizedName(SimpleWine.MODID + ".seed_soybean");
+        this.setCreativeTab(CommonProxy.simpleWineCreativeTabs);
+    }
+
+    @Override
+    public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+        return EnumPlantType.Crop;
+    }
+
+    @Override
+    public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
+        return BlockLoader.cropsSoybean.getDefaultState();
+    }
+
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        ItemStack itemstack = player.getHeldItem(hand);
+        net.minecraft.block.state.IBlockState state = worldIn.getBlockState(pos);
+        if (facing == EnumFacing.UP && player.canPlayerEdit(pos.offset(facing), facing, itemstack) && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up())) {
+            worldIn.setBlockState(pos.up(), BlockLoader.cropsSoybean.getDefaultState());
+            itemstack.shrink(1);
+            return EnumActionResult.SUCCESS;
+        } else {
+            return EnumActionResult.FAIL;
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
